d1bddfa772838d7ff5a4614e08078ddbedff78e5
anplv <vslv.anpilov@gmail.com>
2024-07-13 00:35:22 +0400

    Attempt 2

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -3,11 +3,14 @@ package school.mjc.stage0.module3.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         int first = 1, second = 10, third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
         int linkToFirst = first, linkToSecond = second, linkToThird = third;
         first = 15;
         second = 6;
         third = 4;
-
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
